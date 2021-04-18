72b7b27dd63a6daaedcbc1918d10a45de428cd17
ryuto1246 <ryutonakamura1246@gmail.com>
2021-04-19 00:20:01 +0900

    [add] contact with email, basic info in the contact form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@
   //ボタンを開く
   Array.from(document.getElementsByClassName("btn-title")).forEach((btn, i) => {
     btn.addEventListener("click", () => {
-      Array.from(document.getElementsByClassName("btn"))[i].classList.toggle("open");
+      Array.from(document.getElementsByClassName("btn"))[i].classList.toggle("closed");
     })
   })
+
+  //radioが選択されたらフォームを表示する
+  const showForm = (name, target) => {
+    let className = document.querySelector(`input:checked[name=${name}]`) ? document.querySelector(`input:checked[name=${name}]`).value : "closed";
+    document.getElementById(target).classList = className;
+    Array.from(document.getElementsByName(name)).forEach(el => {
+      el.addEventListener("click", () => {
+        document.getElementById(target).classList = el.value;
+      });
+    });
+  }
+  
+  showForm("senderType", "basic-info");
 })();
